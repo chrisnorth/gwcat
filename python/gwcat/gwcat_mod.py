5b46636a0f1fbd4feeea9dbc9bbb3373449ccc0c
Chris North <chris.north@astro.cf.ac.uk>
2018-11-07 23:16:30 +0000

    Added functions to read from CSV [needs testing]

--- python/gwcat/gwcat_mod.py
+++ python/gwcat/gwcat_mod.py
@@ -2,69 +2,168 @@ import json
 import pandas as pd
 import numpy as np
 
+def compareElement(el1,el2,verbose=False):
+    if type(el1)!=type(el2):
+        # types are different
+        if verbose: print('inconsistent types [{},{}]'.format(type(el1),type(el2)))
+        return(False)
+    elif type(el1)==dict and type(el2)==dict:
+        for el in el1:
+            if el in el2:
+                if verbose: print('Comparing elements {}'.format(el))
+                return(compareElement(el1[el],el2[el],verbose=verbose))
+            else:
+                if verbose: print('element {} not in dict 2'.format(el))
+                return(False)
+        for el in el2:
+            if el not in el1:
+                if verbose: print('element {} not in dict 1'.format(el))
+                return(False)
+    else:
+        try:
+            # if verbose: print('Comparing elements {}'.format(el1))
+            if el1==el2:
+                if verbose: print('{}=={}'.format(el1,el2))
+                return(True)
+            else:
+                if verbose: print('{}!={}'.format(el1,el2))
+                return(False)
+        except:
+            print('ERROR: unable to compare [{} , {}] / [{},{}]'.format(el1,el2),type(el1),type(el2))
+            return()
+
+    return()
+
+def dataframe2jsonEvent(evIn,params,verbose=False):
+    # convert dataframe element to json
+    evOut={}
+    for p in range(len(params)):
+        param=params[p]
+        if verbose: print('Converting value {}'.format(param))
+        if param in evIn:
+            try:
+                pv=evIn[param+'_valtype']
+            except:
+                if verbose: print('ERROR: No valtype found for {}'.format(param))
+                pass
+            try:
+                np.isnan(pv)
+                pass
+            except:
+                evOut[param]={}
+            if pv=='value':
+                evOut[param]['best']=evIn[param]
+            elif pv=='bestfit':
+                evOut[param]['best']=evIn[param]
+                try:
+                    evOut[param]['err']=[evIn[param+'_errp'],evIn[param+'_errm']]
+                except:
+                    if verbose: print('WARNING: no err found form {}'.format(param))
+            elif pv=='range':
+                try:
+                    evOut[param]['lim']=[evIn[param+'_errp'],evIn[param+'_errm']]
+                except:
+                    if verbose: print('WARNING: no lim found form {}'.format(param))
+            elif pv=='upper':
+                evOut[param]['upper']=evIn[param]
+            elif pv=='lower':
+                evOut[param]['lower']=evIn[param]
+            else:
+                if verbose: print('ERROR: unknown type for {} [{}]'.format(param,pv))
+    return(evOut)
+
 class GWCat(object):
     def __init__(self,fileIn='../data/events.json'):
         eventsIn=json.load(open(fileIn))
         self.data=eventsIn['data']
         self.datadict=eventsIn['datadict']
+        self.cols=list(self.datadict.keys())
         self.links=eventsIn['links']
-        self.json2dataframe()
+        # self.json2dataframe()
         return
 
     def json2dataframe(self):
         data=self.data
-        ddIn=self.datadict
-        ddOut={}
         dataOut={}
         series={}
-        ddSeries={}
-        self.cols=['Mchirp','M1','M2','Mtotal','Mfinal','Mratio','DL','z','deltaOmega','chi','af','Erad','lpeak','rho','FAR','UTC','GPS']
+
+        units=pd.DataFrame(self.datadict).transpose()
+        self.units=units
+
         for d in self.cols:
-            ddOut[d]={}
-            if 'unit_en' in ddIn[d]:
-                ddOut[d]['unit']=ddIn[d]['unit_en']
-            if 'name_en' in ddIn[d]:
-                ddOut[d]['description']=ddIn[d]['name_en']
-        for e in data:
-            dataOut[e]={}
-            for d in self.cols:
+            dataOut[d]={}
+            dataOut[d+'_valtype']={}
+            dataOut[d+'_errp']={}
+            dataOut[d+'_errm']={}
+            for e in data:
                 if d not in data[e]:
                     continue
                 if 'best' in data[e][d]:
-                    dataOut[e][d]=data[e][d]['best']
+                    dataOut[d][e]=data[e][d]['best']
                     if 'err' in data[e][d]:
-                        dataOut[e][d+'_valtype']='err'
-                        dataOut[e][d+'_errp']=data[e][d]['err'][0]
-                        dataOut[e][d+'_errm']=data[e][d]['err'][1]
-                    elif 'lim' in data[e][d]:
-                        dataOut[e][d+'_valtype']='range'
-                        dataOut[e][d+'_errp']=data[e][d]['lim'][0]
-                        dataOut[e][d+'_errm']=data[e][d]['lim'][1]
+                        dataOut[d+'_valtype'][e]='bestfit'
+                        dataOut[d+'_errp'][e]=data[e][d]['err'][0]
+                        dataOut[d+'_errm'][e]=data[e][d]['err'][1]
+                    else:
+                        dataOut[d+'_valtype'][e]='value'
+                elif 'lim' in data[e][d]:
+                    dataOut[d+'_valtype'][e]='range'
+                    dataOut[d+'_errp'][e]=data[e][d]['lim'][0]
+                    dataOut[d+'_errm'][e]=data[e][d]['lim'][1]
                 elif 'lower' in data[e][d]:
-                    dataOut[e][d]=data[e][d]['lower']
-                    dataOut[e][d+'_valtype']='lower_limit'
+                    dataOut[d+'_valtype'][e]='lower'
+                    dataOut[d][e]=data[e][d]['lower']
                 elif 'upper' in data[e][d]:
-                    dataOut[e][d]=data[e][d]['upper']
-                    dataOut[e][d+'_valtype']='upper'
-
-        for d in ddOut:
-            ddSeries[d]=pd.Series(ddOut[d],index=ddOut[d].keys())
-        # ddSeries['description']=pd.Series(ddOut['description'],index=ddOut['description'].keys())
-
-        ddFrame=pd.DataFrame(ddSeries)
-        self.units=ddFrame.T
+                    dataOut[d][e]=data[e][d]['upper']
+                    dataOut[d+'_valtype'][e]='upper'
 
         # rows=['description','unit']
-        rows=[]
-        for e in data:
-            series[e]=pd.Series(dataOut[e],index=dataOut[e].keys())
-            rows.append(e)
+        # rows=[]
+        for d in dataOut:
+            series[d]=pd.Series(dataOut[d],index=dataOut[d].keys())
+            # rows.append(d)
 
         df=pd.DataFrame(series)
         dfT=df.T
-        self.dataframe=dfT
+        self.events=dfT
 
-        return
+        return(df.T,units)
+
+    def dataframe2json(self,dataIn,unitsIn,verbose=False):
+        paramsIn=unitsIn.index.tolist()
+        eventsIn=dataIn.index.tolist()
+
+        eventsInDict=dataIn.to_dict(orient='index')
+        unitsInDict=unitsIn.to_dict(orient='index')
+
+        format
+        for param in unitsInDict:
+            # check parameters are in current database
+            if param not in self.datadict:
+                print('Adding parameter %s'%(param))
+                self.datadict[param]=unitsInDict[param]
+
+        for ev in eventsInDict:
+            if verbose: print('merging event {}'.format(ev))
+            event=dataframe2jsonEvent(eventsInDict[ev],paramsIn,verbose=verbose)
+            if ev not in self.data:
+                print('Adding event %s'%(ev))
+                self.data[ev]={}
+            else:
+                print('Comparing events {}'.format(ev))
+                for el in event:
+                    if el not in self.data[ev]:
+                        print ('Adding value {}'.format(el))
+                        self.data[ev][el]=event[el]
+                    else:
+                        if verbose: print('Comapring element {}'.format(el))
+                        if not compareElement(self.data[ev][el],event[el],verbose=verbose):
+                            if verbose: print('Updating value {}'.format(el))
+                            self.data[ev][el]=event[el]
+                        else:
+                            if verbose: print('Keeping value {}'.format(el))
+        return()
+
 
     def getValue(self,event,param,value):
         try:
@@ -74,40 +173,18 @@ class GWCat(object):
             return np.NaN
 
     def exportCSV(self,datafileout,dictfileout=None):
-        # rows=['description','unit']
-        rows=[]
-        for e in self.data:
-            rows.append(e)
-        print(rows)
-        colsAll=[]
-        for c in self.cols:
-            # print c
-            try:
-                self.dataframe[c]
-                colsAll.append(c)
-            except:
-                pass
-            try:
-                self.dataframe[c+'_valtype']
-                colsAll.append(c+'_valtype')
-            except:
-                pass
-            try:
-                self.dataframe[c+'_errp']
-                colsAll.append(c+'_errp')
-            except:
-                pass
-            try:
-                self.dataframe[c+'_errm']
-                colsAll.append(c+'_errm')
-            except:
-                pass
-        # print colsAll
-        pd.DataFrame(self.dataframe,index=rows).to_csv(datafileout,columns=colsAll,encoding='utf8',index_label='Event')
+        dataframe,units = self.json2dataframe()
+        dataframe.transpose().to_csv(datafileout,encoding='utf8',index_label='Event')
 
         if dictfileout!=None:
-            pd.DataFrame(self.units,index=self.cols).to_csv(dictfileout,columns=['description','unit'],encoding='utf8',index_label='Parameter')
+            units.to_csv(dictfileout,encoding='utf8',index_label='Parameter')
         return
 
-    # def importCSV(self,filein):
-
+    def importCSV(self,datafilein,dictfilein=None):
+        # read csv file
+        datain=pd.read_csv(datafilein,index_col=0)
+        if dictfilein!=None:
+            unitsin=pd.read_csv(dictfilein,index_col=0)
+        else:
+            unitsin=self.units
+        self.dataframe2json(datain,unitsin,verbose=True)
